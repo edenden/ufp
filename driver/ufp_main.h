6ac35ff8b7fea3360bd4610b7ce84a4e937247f5
Yukito Ueno <eden@sfc.wide.ad.jp>
2016-07-03 21:06:54 +0900

    get rid of device dependent code

--- driver/ufp_main.h
+++ driver/ufp_main.h
@@ -9,15 +9,12 @@
 #undef pr_fmt
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#define EVENTFD_INCREMENT		1
+#define EVENTFD_INCREMENT	1
 
 enum ufp_irq_type {
 	IXMAP_IRQ_RX = 0,
 	IXMAP_IRQ_TX,
 };
-
-struct ufp_mac_info;
-struct ufp_mbx_info;
 
 struct ufp_hw {
 	u8 __iomem		*hw_addr; /* unused */
@@ -26,7 +23,6 @@ struct ufp_hw {
 	u16			subsystem_device_id;
 	u16			vendor_id;
 	u8			revision_id;
-	int			api_version;
 };
 
 struct ufp_port {
@@ -45,7 +41,6 @@ struct ufp_port {
 	unsigned long		iobase;
 	unsigned long		iolen;
 	struct ufp_hw		*hw;
-	char			eeprom_id[32];
 
 	struct msix_entry	*msix_entries;
 	uint32_t		num_q_vectors;
